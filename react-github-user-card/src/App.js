7bdc158eea8f854a71198b0a3ffe06faf8593447
Vicky <vyue013@gmail.com>
2019-08-06 15:05:34 -0700

    displayed user GitHub data with styled user card

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import axios from 'axios';
+import UserCard from './components/UserCard.js';
 
 import './App.css';
 
@@ -7,15 +8,51 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: ''
+      user: [],
+      errorMessage: ''
     };
   }
 
+  componentDidMount() {
+    this.fetchUser();
+  };
+
+  handleUserChange = e => {
+    this.setState({ user: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.fetchUser();
+  };
+
+  fetchUser = () => {
+    axios.get("https://api.github.com/users/llamousse")
+      .then(response => {
+        console.log("user data", response.data)
+        this.setState({ user: response.data })
+        console.log(this.state.user)
+      })
+      // .then(response => {
+      //   this.setState({ user: response.data.login })
+      // })
+      .catch(err => {
+        console.log("Error: Something went wrong", err);
+        this.setState({ user: '' })
+        this.setState({ errorMessage: "user not found" })
+      })
+  }
+
   render() {
-    return (
-      <div>
-        Hello World!
-      </div>
+    return (       
+      <UserCard
+        image={this.state.user.avatar_url}
+        header={this.state.user.login}
+        meta={this.state.user.name}
+        description={this.state.user.bio}
+        extra={this.state.user.followers}
+        followersUrl={this.state.user.login}
+      />
     );
   }
 }
